5a0e7d844e97437d9cdb0fceeb3ef27f11491545
cameronjacoby <cameron.jacoby@gmail.com>
2016-01-18 19:22:52 -0800

    implement books show route

--- app.js
+++ app.js
@@ -7,8 +7,18 @@ var app = angular.module('libraryApp', ['ngRoute']);
 app.config(function($routeProvider, $locationProvider)  {
   $routeProvider
     .when('/', {
-      templateUrl: 'templates/books/index.html',
+      templateUrl: '/templates/books/index.html',
       controller: 'BooksIndexCtrl'
+    })
+    .when('/books/:id', {
+      templateUrl: '/templates/books/show.html',
+      controller: 'BooksShowCtrl'
+    });
+
+  $locationProvider
+    .html5Mode({
+      enabled: true,
+      requireBase: false
     });
 });
 
@@ -17,7 +27,7 @@ app.config(function($routeProvider, $locationProvider)  {
 /////////////////
 
 app.controller('BooksIndexCtrl', ['$scope', function ($scope) {
-  $scope.booksIndexTest = 'Connected to BooksIndexCtrl';
+  $scope.books = allBooks;
 }]);
 
 app.controller('BooksShowCtrl', ['$scope', function ($scope) {
